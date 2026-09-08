f620f9905a9451f5e5f46976bcc5e2d4f602c4d6
morindat <papaadennis@gmail.com>
2026-09-08 13:18:39 +0530

    Added coinChangeII

--- coinChangeII.cpp
+++ coinChangeII.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<long long> dp(amount + 1, 0);
+        vector<unsigned long long> dp(amount + 1, 0);
         dp[0] = 1;
 
         for (int coin : coins) {
